f73b7f4534ef6a5c5c7d61b3e80b54a2931fc98e
njorogetimk <njorogetimk@gmail.com>
2023-08-17 20:53:40 +0300

    Add a database

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 from src.Blueprints.auth import auth
@@ -5,11 +6,23 @@ from src.Blueprints.admin import admins
 from src.Blueprints.clients import clients
 from src.Blueprints.readers import readers
 
+from src.models import db
 
-def create_app():
+
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
+    if not test_config:
+        app.config.from_mapping(
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        )
     
+    else:
+        app.config.from_mapping(test_config)
+    
+    db.app = app
+    db.init_app(app)
    
     app.register_blueprint(auth)
     app.register_blueprint(admins)
---
+++ src/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    role = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.username}, Role: {self.role}>'
+
+
+
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    current_reading = db.Column(db.Integer, nullable=False)
+    read_on = db.Column(db.DateTime(), nullable=False)
+    reader = db.Column(db.String(100), nullable=False)
+    client = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Bill: {self.current_reading}>'
