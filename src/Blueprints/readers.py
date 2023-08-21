105161a787ecc821f65466a13806ab95e3f110e9
njorogetimk <njorogetimk@gmail.com>
2023-08-21 10:40:12 +0300

    Remove unncessary routes

--- src/Blueprints/readers.py
+++ src/Blueprints/readers.py
@@ -1,17 +1,8 @@
 from flask import Blueprint
 
-readers = Blueprint('readers', __name__, url_prefix='/readers/v1/')
+readers = Blueprint("readers", __name__, url_prefix="/readers/v1/")
 
 
-@readers.route('/')
-def get_readers():
-    return '<h1>All readers</h1>'
-
-@readers.route('/reader/<int:reader_id>')
-def get_reader(reader_id):
-
-    return f'<h1>reader {reader_id}</h1>'
-
-@readers.route('/read-meter')
+@readers.route("/read-meter")
 def read_meter():
-    return f'<h1>Read Meter</h1>'
+    return f"<h1>Read Meter</h1>"
