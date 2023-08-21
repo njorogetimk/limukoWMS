657b5a52fd2d8ddae8871c0a452b90235a00c903
njorogetimk <njorogetimk@gmail.com>
2023-08-21 13:34:35 +0300

    Add a read meter route

--- src/Blueprints/readers.py
+++ src/Blueprints/readers.py
@@ -1,8 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from src.models import Reader, Client, Bill, db
 
 readers = Blueprint("readers", __name__, url_prefix="/readers/v1/")
 
 
-@readers.route("/read-meter")
-def read_meter():
-    return f"<h1>Read Meter</h1>"
+@readers.route("/<int:id>/read-meter", methods=["POST", "GET"])
+def read_meter(id):
+    # Reader's id
+    reader = Reader.query.get_or_404(id)
+    # If not reader do nothing
+
+    if request.method == "POST":
+        client_id = request.form.get("client_id")
+        current_reading = request.form.get("current_reading")
+
+        client = Client.query.get_or_404(client_id)
+
+        bill = Bill(
+            current_reading=current_reading, client_id=client.id, reader_id=reader.id
+        )
+        db.session.add(bill)
+        db.session.commit()
+
+        return redirect(url_for("readers.read_meter", id=reader.id))
+
+    return render_template("read_meter.html")
