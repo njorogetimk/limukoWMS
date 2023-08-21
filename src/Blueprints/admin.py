4358f12437ddaa56e09a8def4fa7fc65f68530a3
njorogetimk <njorogetimk@gmail.com>
2023-08-21 08:29:33 +0300

    Commit first admin template

--- src/Blueprints/admin.py
+++ src/Blueprints/admin.py
@@ -1,61 +1,61 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
-admins = Blueprint("admins", __name__, url_prefix="/admins/v1")
+admin = Blueprint("admin", __name__, url_prefix="/admin/v1")
 
 
-@admins.route("/")
+@admin.route("/")
 def get_admins():
-    return "<h1>All Admins</h1>"
+    return render_template("admin.html")
 
 
-@admins.route("/admin/<int:admin_id>")
+@admin.route("/admin/<int:admin_id>")
 def get_admin(admin_id):
-    return f"<h1>admin {admin_id}</h1>"
+    return render_template("admin.html")
 
 
-@admins.route("/add-admin")
+@admin.route("/add-admin")
 def add_admin():
     return f"<h1>Add admin</h1>"
 
 
-@admins.route("/modify-admin/<int:admin_id>")
+@admin.route("/modify-admin/<int:admin_id>")
 def modify_admin(admin_id):
     return f"<h1>Modify admin</h1>"
 
 
-@admins.route("/delete-admin/<int:admin_id>")
+@admin.route("/delete-admin/<int:admin_id>")
 def delete_admin(admin_id):
     return f"<h1>Delete admin</h1>"
 
 
 # Add modify and delete clients
-@admins.route("/add-client")
+@admin.route("/add-client")
 def add_client():
     return f"<h1>Add Client</h1>"
 
 
-@admins.route("/modify-client/<int:client_id>")
+@admin.route("/modify-client/<int:client_id>")
 def modify_client(client_id):
     return f"<h1>Modify Client</h1>"
 
 
-@admins.route("/delete-client/<int:client_id>")
+@admin.route("/delete-client/<int:client_id>")
 def delete_client(client_id):
     return f"<h1>Delete Client</h1>"
 
 
 # Add Edit and delete meter readers
-@admins.route("/add-reader")
+@admin.route("/add-reader")
 def add_reader():
     return f"<h1>Add reader</h1>"
 
 
-@admins.route("/modify-reader/<int:reader_id>")
+@admin.route("/modify-reader/<int:reader_id>")
 def modify_reader(reader_id):
     return f"<h1>Modify reader</h1>"
 
 
-@admins.route("/delete-reader/<int:reader_id>")
+@admin.route("/delete-reader/<int:reader_id>")
 def delete_reader(reader_id):
     return f"<h1>Delete reader</h1>"
--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 from src.Blueprints.auth import auth
-from src.Blueprints.admin import admins
+from src.Blueprints.admin import admin
 from src.Blueprints.clients import clients
 from src.Blueprints.readers import readers
 
@@ -14,18 +14,18 @@ def create_app(test_config=None):
 
     if not test_config:
         app.config.from_mapping(
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI'),
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
         )
-    
+
     else:
         app.config.from_mapping(test_config)
-    
+
     db.app = app
     db.init_app(app)
-   
+
     app.register_blueprint(auth)
-    app.register_blueprint(admins)
+    app.register_blueprint(admin)
     app.register_blueprint(clients)
     app.register_blueprint(readers)
 
