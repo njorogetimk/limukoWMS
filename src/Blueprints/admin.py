c481591b8234877131ca1eaf93ad52e2dbf3fc58
njorogetimk <njorogetimk@gmail.com>
2023-08-21 10:41:25 +0300

    Include administrator functionalities to add, view and delete users

--- src/Blueprints/admin.py
+++ src/Blueprints/admin.py
@@ -1,61 +1,68 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from src.models import User, db
 
 
 admin = Blueprint("admin", __name__, url_prefix="/admin/v1")
 
 
-@admin.route("/")
-def get_admins():
-    return render_template("admin.html")
+@admin.route("/users/<int:role>")
+def get_users(role):
+    if role == 0:
+        users = User.query.all()
+        return render_template("users.html", users=users, role=0)
+
+    elif role == 1:
+        users = User.query.filter_by(role=1).all()
+        return render_template("users.html", users=users, role=1)
+
+    elif role == 2:
+        users = User.query.filter_by(role=2).all()
+        return render_template("users.html", users=users, role=2)
+
+    elif role == 3:
+        users = User.query.filter_by(role=3).all()
+        return render_template("users.html", users=users, role=3)
+
+    else:
+        return "<h1>Wrong Number</h1>"
 
 
-@admin.route("/admin/<int:admin_id>")
+@admin.route("/<int:admin_id>")
 def get_admin(admin_id):
     return render_template("admin.html")
 
 
-@admin.route("/add-admin")
-def add_admin():
-    return f"<h1>Add admin</h1>"
+@admin.route("/add-user", methods=["POST", "GET"])
+def add_user():
+    if request.method == "POST":
+        username = request.form.get("username")
+        role = request.form.get("role")
+        password1 = request.form.get("password1")
+
+        user = User(username=username, role=role, password=password1)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("admin.get_users", role=role))
+
+    return render_template("add_user.html")
 
 
-@admin.route("/modify-admin/<int:admin_id>")
-def modify_admin(admin_id):
-    return f"<h1>Modify admin</h1>"
+@admin.route("/user/<int:id>")
+def get_user(id):
+    user = User.query.get_or_404(id)
+
+    return render_template("user.html", user=user)
 
 
-@admin.route("/delete-admin/<int:admin_id>")
-def delete_admin(admin_id):
-    return f"<h1>Delete admin</h1>"
+@admin.post("/delete/<int:id>")
+def delete_user(id):
+    user = User.query.get_or_404(id)
 
+    db.session.delete(user)
+    db.session.commit()
 
-# Add modify and delete clients
-@admin.route("/add-client")
-def add_client():
-    return f"<h1>Add Client</h1>"
+    users = User.query.all()
 
-
-@admin.route("/modify-client/<int:client_id>")
-def modify_client(client_id):
-    return f"<h1>Modify Client</h1>"
-
-
-@admin.route("/delete-client/<int:client_id>")
-def delete_client(client_id):
-    return f"<h1>Delete Client</h1>"
-
-
-# Add Edit and delete meter readers
-@admin.route("/add-reader")
-def add_reader():
-    return f"<h1>Add reader</h1>"
-
-
-@admin.route("/modify-reader/<int:reader_id>")
-def modify_reader(reader_id):
-    return f"<h1>Modify reader</h1>"
-
-
-@admin.route("/delete-reader/<int:reader_id>")
-def delete_reader(reader_id):
-    return f"<h1>Delete reader</h1>"
+    return redirect(url_for("admin.get_users", role=user.role))
