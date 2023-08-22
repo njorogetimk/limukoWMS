71d5a18297a7de845478ba56455f5b7de1372f32
njorogetimk <njorogetimk@gmail.com>
2023-08-22 17:02:32 +0300

    Implement add-user form with flask WTF

--- src/forms.py
+++ src/forms.py
@@ -1,18 +1,38 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SelectField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     role = SelectField(
-        "Role",
+        "Select Role",
         validators=[DataRequired()],
         choices=[
-            ("choose disabled", "Select your role"),
             ("admin", "Admin"),
             ("reader", "Reader"),
         ],
     )
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
+
+
+class AddUserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    role = SelectField(
+        "Select Role",
+        validators=[DataRequired()],
+        choices=[
+            ("admin", "Admin"),
+            ("reader", "Reader"),
+            ("client", "Client"),
+        ],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            EqualTo("verify_password", message="Passwords must match"),
+        ],
+    )
+    verify_password = PasswordField("Confirm Password", validators=[DataRequired()])
