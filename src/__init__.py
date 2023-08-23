7e1f1e8fb59931f6a4ac30a2860d8e8193f7152e
njorogetimk <njorogetimk@gmail.com>
2023-08-23 22:57:36 +0300

    Initial docker commit

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, redirect, url_for, render_template
 from flask_login import LoginManager
+from dotenv import load_dotenv
 
 from src.Blueprints.auth import auth
 from src.Blueprints.admin import admin
@@ -11,7 +12,7 @@ from src.models import db, Admin, Reader, AnonymousUser
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
+    load_dotenv()
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY"),
         SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
