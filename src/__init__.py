03bfe55b82d871b5edb67fcc7072d5bb290a1015
njorogetimk <njorogetimk@gmail.com>
2023-08-23 08:28:31 +0300

    Remove test config settings

--- src/__init__.py
+++ src/__init__.py
@@ -9,18 +9,14 @@ from src.Blueprints.readers import readers
 from src.models import db, Admin, Reader, AnonymousUser
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if not test_config:
-        app.config.from_mapping(
-            SECRET_KEY=os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
-            SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        )
-
-    else:
-        app.config.from_mapping(test_config)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
+        SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
 
     db.app = app
     db.init_app(app)
